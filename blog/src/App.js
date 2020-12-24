f0dba5192b52a06a4939404972abd4a3e8a05205
harsh.atdrive <54494999+harsh-singh-vst-au4@users.noreply.github.com>
2020-12-24 15:51:11 +0530

    updated changes

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,124 +1,51 @@
-import React, { Component } from "react";
-import { database } from "./firebase";
-import _ from 'lodash';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, { useState } from 'react';
+import { Typography, Button, Form, message } from 'antd';
+import QuillEditor from './editor';
+import ReactHtmlParser from 'react-html-parser';
 
+const { Title } = Typography
 
-class App extends Component{
-  state = {
-    title: '',
-    body: '',
-    posts: {},  
+function App(props) {
+
+  const [content, setContent] = useState("")
+  const [files, setFiles] = useState([]);
+
+  const onEditorChange = (value) => {
+    setContent(value)
   }
 
-  textInput = null;
-
-  componentDidMount() {
-    database.on('value', snapshot => {
-      this.setState({
-        posts: snapshot.val()
-      })
-    })
+  const onFilesChange = (value) => {
+    setFiles(files)
   }
 
-  onInputChange =({ target }) => {
-    const {name,value} = target
-    this.setState({
-      [name]: value
-    })    
-  }
-
-  onHandleChange = (e) => {
-    this.setState({body: e})
-  }
-
-  setTextInputRef = element => {
-    this.textInput = element;
-  };
-
-  onHandleSubmit = (e) => {
-    e.preventDefault();
-    const post = {
-      title: this.state.title,
-      body: this.state.body
-    };
-    //console.log(post)
-    database.push(post)
-    alert('Data gayo')
-    this.resetUserInputs();
-  }
-
-  resetUserInputs = () => {
-    this.setState({
-      
-      title: '',
-      body: '',
-      answer: ''
-    });
-  }
-
-  renderPosts() {
-    return _.map(this.state.posts,(post,key)=> {
-      return (
-      <div key={key}>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
+  return (
+    <div style={{ maxWidth: '60%', maxHeight: '60%',margin: '10px auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <Title level={2}>Editor</Title>
       </div>
-      )
-    })
-  }
 
-  render(){
-    return (
-      <div className="container">
-        <form onSubmit={this.onHandleSubmit}>
-          <div className="form-group">
-          <input type="text" 
-            placeholder="Title" 
-            value={this.state.title}
-            name="title"
-            onChange={this.onInputChange}  
-            ref={this.setTextInputRef}
-            className="form-control" 
-            required/>
-          </div>
-          <div className="form-group">
-          <ReactQuill 
-          modules={App.modules} 
-          formats={App.formats}
-            placeholder="Body" 
-            name="body"
-            value={this.state.body} 
-            onChange={this.onHandleChange} 
-          />
-          </div>
-          <div className="form-group">
-          <button className="btn btn-primary" >Submit</button>
-          </div>
-        </form>
-      <br/>
-      {this.renderPosts()}
-      </div>
-    )
-  }
+      <QuillEditor
+        placeholder="What's on your Mind Today"
+        onEditorChange={onEditorChange}
+        onFilesChange={onFilesChange}
+      />
+
+      <Form >
+        <div style={{ textAlign: 'center', margin: '10px auto' }}>
+          <Button
+            size="large"
+            htmlType="submit"
+            className=""
+            // onSubmit={onSubmit}
+          >
+            Submit
+          </Button>
+        </div>
+      </Form>
+      {/* {content} */}
+      {ReactHtmlParser(content)}
+    </div>
+  )
 }
 
-App.modules = {
-  toolbar: [
-    [{header:'1'},{header:'2'},{header:'3'},{font:[]}],
-    [{size: []}],
-    ['bold','italic','underline', 'strike', 'blockquote'],
-    [{'list': 'ordered'},{'list': 'bullet'}],
-    ['link','image','video'],
-    ['clean'],
-    ['code-block']
-  ]
-}
-
-App.formats = [
-  'header', 'font', 'size','bold','italic','underline',
-  'strike', 'blockquote','list','ordered','bullet',
-  'link', 'image','video', 'code-block'
-]
 export default App;
